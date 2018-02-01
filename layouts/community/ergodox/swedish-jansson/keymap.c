19411862680494967fa071bd250bbccc2c00a588
Christoffer Jansson <cjansss@gmail.com>
2018-02-01 15:40:15 +0100

    initial mac layer support

--- layouts/community/ergodox/swedish-jansson/keymap.c
+++ layouts/community/ergodox/swedish-jansson/keymap.c
@@ -40,7 +40,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * | Left   |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ;  |   :  |_/Ctrl| RShift |
    * | Shift  |      |      |      |      |      |      |           |      |      |      |   ,  |   .  |-     |        |
    * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
-   *   | '/L1 |  `   |AltShf| Left | Right|                                       |  Up  | Down |  ^   |  *   | ~L1  |
+   *   | '/L1 |  `   | LGUI | Left | Right|                                       |  Up  | Down |  ^   |  *   | ~L1  |
    *   |      |  '   |      |      |      |                                       |      |      |      |      |      |
    *   `----------------------------------'                                       `----------------------------------'
    *                                        ,-------------.       ,-------------.
@@ -53,14 +53,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    */
   [BASE] = LAYOUT_ergodox(
     // left hand
-    KC_ESC,            KC_1,    KC_2,          KC_3,    KC_4,    KC_5, KC_LEFT,
-    KC_DELT,           KC_Q,    KC_W,          KC_E,    KC_R,    KC_T, KC_FN2,
-    KC_LCTL,           KC_A,    KC_S,          KC_D,    KC_F,    KC_G,
-    KC_LSFT,           KC_Z,    KC_X,          KC_C,    KC_V,    KC_B, ALL_T(KC_NO),
-    LT(SYMB, NO_APOS), NO_ACUT, LALT(KC_LSFT), KC_LEFT, KC_RGHT,
-                                                                 ALT_T(KC_APP),   KC_LGUI,
-                                                                                  KC_HOME,
-                                                                 KC_SPC, KC_BSPC, KC_END,
+    KC_ESC,            KC_1,    KC_2,    KC_3,    KC_4,    KC_5, KC_LEFT,
+    KC_DELT,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T, KC_FN2,
+    KC_LCTL,           KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
+    KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B, KC_FN3,
+    LT(SYMB, NO_APOS), NO_ACUT, KC_LGui, KC_LEFT, KC_RGHT,
+                                                  ALT_T(KC_APP),   KC_LGUI,
+                                                                   KC_HOME,
+                                                  KC_SPC, KC_BSPC, KC_END,
     // right hand
     KC_RGHT, KC_6,   KC_7,   KC_8,    KC_9,    KC_0,              NO_PLUS,
     KC_DELT, KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,              NO_AA,
@@ -161,12 +161,54 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_PGUP,
     KC_PGDN, KC_TAB, KC_ENT
   ),
-};
+
+ /* Keymap 3: Mac Symbol Layer
+   *
+   * ,--------------------------------------------------.           ,--------------------------------------------------.
+   * |  ½ §   |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
+   * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+   * |        |   !  |   @  |   {  |   }  |   |  |   \  |           |      |   Up |   7  |   8  |   9  |   *  |   F12  |
+   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+   * |        |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   +  |        |
+   * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+   * |        |   %  |   ^  |   [  |   ]  |   ~  |  fn  |           |      |   &  |   1  |   2  |   3  |   \  |        |
+   * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+   *   |      |      |      |      |      |                                       |      |    . |   0  |   =  |      |
+   *   `----------------------------------'                                       `----------------------------------'
+   *                                        ,-------------.       ,-------------.
+   *                                        |      | Play |       | Mute |      |
+   *                                 ,------|------|------|       |------+------+------.
+   *                                 |      |      | Next |       | VolUp|      |      |
+   *                                 |   <  |      |------|       |------|      |  >   |
+   *                                 |      |      | Prev |       | Voldn|      |      |
+   *                                 `--------------------'       `--------------------'
+   */
+  [MAC] = LAYOUT_ergodox(
+    // left hand
+    NO_HALF, KC_F1,   KC_F2,   KC_F3,       KC_F4,       KC_F5,   KC_TRNS,
+    KC_TRNS, KC_EXLM, NO_AT_MAC,   NO_LCBR_MAC, NO_RCBR_MAC, NO_PIPE_MAC, KC_PAST,
+    KC_LCTL, KC_HASH, NO_DLR_MAC,  NO_LPRN,     NO_RPRN,     NO_GRV_MAC,
+    KC_LSFT, KC_PERC, NO_CIRC, NO_LBRC,     NO_RBRC,     NO_TILD, OSL(2),
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,     KC_TRNS,
+                                                 KC_TRNS, KC_MPLY,
+                                                          KC_MNXT,
+                                        KC_FN8, KC_TRNS, KC_MPRV,
+    // right hand
+    KC_TRNS, KC_F6,   KC_F7,   KC_F8,  KC_F9, KC_F10,  KC_F11,
+    KC_TRNS, KC_UP,   KC_7,    KC_8,   KC_9,  NO_ASTR, KC_F12,
+             KC_DOWN, KC_4,    KC_5,   KC_6,  NO_PLUS, KC_TRNS,
+    KC_TRNS, NO_AMPR, KC_1,    KC_2,   KC_3,  KC_NUBS, KC_TRNS,
+                      KC_TRNS, KC_DOT, KC_0,  NO_EQL,  KC_TRNS,
+    KC_MUTE, KC_TRNS,
+    KC_VOLU,
+    KC_VOLD, KC_TRNS, KC_FN9
+  ),
 
 // Toggle layers.
 const uint16_t PROGMEM fn_actions[] = {
   [1] = ACTION_LAYER_TAP_TOGGLE(SYMB),  // FN1 - Momentary Layer 1 (Symbols)
   [2] = ACTION_LAYER_TAP_TOGGLE(GAME),  // FN2 - Momentary Layer 2 (Game)
+  [2] = ACTION_LAYER_TAP_TOGGLE(MAC),  // FN3 - Momentary Layer 2 (Game)
 };
 
 // Runs constantly in the background, in a loop.
@@ -184,6 +226,8 @@ void matrix_scan_user(void) {
       // Changed this to the blue led since it's less bright after using a permament marker.
       ergodox_right_led_3_on();
       break;
+    case 3:
+      ergodox_right_led_2_on();
     default:
       // none
       break;
